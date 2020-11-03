03b424cec18960e4b7ee6550c9d2e6ed5f67fdac
Quinlan Jager <quinlan.jager@gmail.com>
2020-11-02 16:19:06 -0800

    Update tags

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SentryLogCapture.Mixfile do
   def project do
     [
       app: :sentry_log_capture,
-      version: "0.1.6",
+      version: "0.0.3",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
